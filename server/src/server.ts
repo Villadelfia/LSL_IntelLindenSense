47d4e13ccafc74121bbefd95e7381aad43c52ead
Randy Thiemann <randy.thiemann@edt.zone>
2023-07-07 10:05:31 +0200

    Boilerplate

---
+++ server/src/server.ts
@@ -0,0 +1,88 @@
+import {
+    createConnection,
+    TextDocuments,
+    Diagnostic,
+    DiagnosticSeverity,
+    ProposedFeatures,
+    InitializeParams,
+    DidChangeConfigurationNotification,
+    CompletionItem,
+    CompletionItemKind,
+    TextDocumentPositionParams,
+    TextDocumentSyncKind,
+    InitializeResult
+} from 'vscode-languageserver/node';
+import { TextDocument } from 'vscode-languageserver-textdocument';
+
+const connection = createConnection(ProposedFeatures.all);
+const documents = new TextDocuments(TextDocument);
+
+let hasConfigurationCapability = false;
+let hasWorkspaceFolderCapability = false;
+let hasDiagnoticRelatedInformationCapability = false;
+
+connection.onInitialize((params: InitializeParams) => {
+    const capabilities = params.capabilities;
+
+    hasConfigurationCapability = !!(capabilities.workspace && !!capabilities.workspace.configuration);
+    hasWorkspaceFolderCapability = !!(capabilities.workspace && !!capabilities.workspace.workspaceFolders);
+    hasDiagnoticRelatedInformationCapability = !!(capabilities.textDocument && capabilities.textDocument.publishDiagnostics && capabilities.textDocument.publishDiagnostics.relatedInformation);
+
+    const result: InitializeResult = {
+        capabilities: {
+            textDocumentSync: TextDocumentSyncKind.Incremental,
+            completionProvider: {
+                resolveProvider: true
+            }
+        }
+    };
+
+    if(hasWorkspaceFolderCapability) {
+        result.capabilities.workspace = {
+            workspaceFolders: {
+                supported: true
+            }
+        };
+    }
+
+    return result;
+});
+
+connection.onInitialized(() => {
+    if(hasConfigurationCapability) {
+        connection.client.register(DidChangeConfigurationNotification.type, undefined);
+    }
+
+    if(hasWorkspaceFolderCapability) {
+        connection.workspace.onDidChangeWorkspaceFolders(_event => {
+            connection.console.log('Workspace folder change event received.');
+        });
+    }
+});
+
+connection.onDidChangeConfiguration(_change => {
+    documents.all().forEach(validateTextDocument);
+});
+
+documents.onDidChangeContent(change => {
+    validateTextDocument(change.document);
+});
+
+async function validateTextDocument(textDocument: TextDocument): Promise<void> {
+    return;
+}
+
+connection.onDidChangeWatchedFiles(_change => {
+    connection.console.log('Watched file change event received.');
+});
+
+connection.onCompletion((_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
+    return [];
+});
+
+connection.onCompletionResolve((item: CompletionItem): CompletionItem => {
+    return item;
+});
+
+documents.listen(connection);
+connection.listen();
